a8105b2569f8694ac071a35223e49e8e672c6757
Ahmed Salim <ahmedsalim3100.as@gmail.com>
2026-07-15 15:22:11 +0300

    Create SaleItem.cs

---
+++ ChemStore.Core/Entities/SaleItem.cs
@@ -0,0 +1,22 @@
+namespace ChemStore.Core.Entities;
+
+public class SaleItem : BaseEntity
+{
+    public int SaleId { get; set; }
+
+    public Sale Sale { get; set; } = null!;
+
+    public int ProductId { get; set; }
+
+    public Product Product { get; set; } = null!;
+
+    public decimal Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public decimal CostPrice { get; set; }
+
+    public decimal Discount { get; set; }
+
+    public decimal Total { get; set; }
+}
