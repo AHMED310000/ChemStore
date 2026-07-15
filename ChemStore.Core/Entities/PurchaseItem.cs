22d0cc4cc370d99ea278cf1c58aa7728b771d1d4
Ahmed Salim <ahmedsalim3100.as@gmail.com>
2026-07-15 15:23:05 +0300

    Create PurchaseItem.cs

---
+++ ChemStore.Core/Entities/PurchaseItem.cs
@@ -0,0 +1,18 @@
+namespace ChemStore.Core.Entities;
+
+public class PurchaseItem : BaseEntity
+{
+    public int PurchaseId { get; set; }
+
+    public Purchase Purchase { get; set; } = null!;
+
+    public int ProductId { get; set; }
+
+    public Product Product { get; set; } = null!;
+
+    public decimal Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public decimal Total { get; set; }
+}
