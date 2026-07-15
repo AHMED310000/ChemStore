4b14b9934907eb9851481d1f58777990ea14e628
Ahmed Salim <ahmedsalim3100.as@gmail.com>
2026-07-15 15:26:47 +0300

    Create ProductService.cs

---
+++ ChemStore.Data/Repositories/ProductService.cs
@@ -0,0 +1,58 @@
+using ChemStore.Core.Entities;
+using ChemStore.Data.Repositories;
+using ChemStore.Services.Interfaces;
+
+namespace ChemStore.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IGenericRepository<Product> _repository;
+
+    public ProductService(IGenericRepository<Product> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<Product?> GetByIdAsync(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task AddAsync(Product product)
+    {
+        await _repository.AddAsync(product);
+        await _repository.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Product product)
+    {
+        await _repository.UpdateAsync(product);
+        await _repository.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var product = await _repository.GetByIdAsync(id);
+
+        if (product == null)
+            return;
+
+        await _repository.DeleteAsync(product);
+        await _repository.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Product>> SearchAsync(string keyword)
+    {
+        keyword = keyword.ToLower();
+
+        return await _repository.FindAsync(x =>
+            x.Name.ToLower().Contains(keyword) ||
+            x.Code.ToLower().Contains(keyword) ||
+            (x.Barcode ?? "").Contains(keyword));
+    }
+}
