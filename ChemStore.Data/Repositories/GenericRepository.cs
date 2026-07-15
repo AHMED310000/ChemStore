072f0b3cc99b6904d05605307143f5ea99fec63f
Ahmed Salim <ahmedsalim3100.as@gmail.com>
2026-07-15 15:26:12 +0300

    Implement Generic Repository pattern for data access

---
+++ ChemStore.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using ChemStore.Data.Context;
+
+namespace ChemStore.Data.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly ChemStoreDbContext _context;
+
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepository(ChemStoreDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+}
