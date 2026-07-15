cb980e78f84712a5994fda0a218dae52a42de2c5
Ahmed Salim <ahmedsalim3100.as@gmail.com>
2026-07-15 15:56:10 +0300

    Create TransactionType.cs

---
+++ ChemStore.Core/Entities/TransactionType.cs
@@ -0,0 +1,18 @@
+namespace ChemStore.Core.Enums;
+
+public enum TransactionType
+{
+    OpeningBalance = 1,
+
+    Purchase = 2,
+
+    PurchaseReturn = 3,
+
+    Sale = 4,
+
+    SaleReturn = 5,
+
+    InventoryAdjustment = 6,
+
+    Transfer = 7
+}
